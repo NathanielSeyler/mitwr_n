a3e233e369697c7b7ca1d6b4c3600eb264fed14f
NathanielSeyler <nathseyler@hotmail.fr>
2015-05-15 11:57:44 +0200

    ajout de la fonction hour et date et modif de la fonction tweets_post

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,32 @@
-function Hour () 
+function Hour() 
 	{
-		d = new Date();
-		h = d.getHours();
-		min = d.getMinutes();
-		s = d.getSeconds();
-		txt = h + ": " + min + ": " + s;
-		Calk.innerHTML = txt;
+		var d = new Date();
+		var h = d.getHours();
+		var min = d.getMinutes();
+		var s = d.getSeconds();
+		var txt = h +" h " + min + " min " + s +" s ";
 		setTimeout("Hour()", 1000);
+		return txt;
 	}
+	
+function Date_auj(){
+	var d = new Date();
+	var j = d.getDay()+1;
+	var m = d.getMonth()+1;
+	var a = d.getYear()+1900;
+	var date_txt = j+'/'+m+'/'+a;
+	return date_txt;
+}
 
 
 function tweett_post() {
+	var h = Hour();
+	var d_auj = Date_auj();
 	var user_name = document.getElementById("user_name").value;
 	var tw_p = document.getElementById("tw_p").value;
 	if (tw_p == "")
       {alert("Il faut ecrire un tweet avant de l'envoyer");}
-    else {document.getElementById("tweets").innerHTML += '<ul>'+'<li>'+user_name+" à écrit à "+Date.now()+':'+'</li>'+'</ul>'+'<p>'+tw_p+'</p>';}
+    else {document.getElementById("tweets").innerHTML += '<ul>'+'<li>'+user_name+" à écrit le "+d_auj+" à "+h+':'+'</li>'+'</ul>'+'<p>'+tw_p+'</p>';}
 }
 
 function verifie_password() {
