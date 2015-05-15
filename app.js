0c2376f9e7aac6a5d6bde1a05edbbffc8b9fd62d
NathanielSeyler <nathseyler@hotmail.fr>
2015-05-15 11:54:08 +0200

    ajout de commentaires

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+//app.get('/inscription', routes.inscription);
+//app.post('/contact', routes.contact);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
