3130b04f1c82f3aaaf648e6038cf91231ba326ef
NathanielSeyler <nathaniel.seyler@etu.unistra.fr>
2015-05-20 21:44:46 +0200

    essaie de model avec mongoose

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. 
+router.get('/users', function(req, res) {
+  mongoose.model('users').find(function(err,users) {
+	  res.send(users);
+  });
+});*/
+
 module.exports = router;
